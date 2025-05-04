9618583b99fd1b37e17a00f39f83423aca5f1fb0
Silliamh78 <har20041@byui.edu>
2025-05-03 21:40:00 -0600

    added light and dark mode buttons

--- mission/mission.js
+++ mission/mission.js
@@ -1,2 +1,18 @@
 let selectElem = documment.querySelector('select');
 let logo = document.querySelector ('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current= selectElem.value;
+    if (current == 'dark'){
+        body.darkmode
+        //change body to dark
+        // change logo to the new logo
+    }else{
+        body
+        //change body to light OR remove dark class from body
+        //change logo back to original logo
+    }
+}
+
